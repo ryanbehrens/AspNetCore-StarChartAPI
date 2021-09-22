bdfc9f40a7bdfd5cac79767a14c44e899bfcc29d
Behrens, Ryan (ESD) <RBehrens@esd.wa.gov>
2021-09-22 12:19:16 -0700

    bug fix

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -14,7 +14,7 @@ namespace StarChart.Models
         public string Name { get; set; }
         public int? OrbitedObject { get; set; }
         [NotMapped]
-        public List<CelestialObject> Satellites { get; set }
+        public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
     }
 }
